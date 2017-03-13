621376d16c4808e4cf10cb5f6e0a54d3260bc7a9
Joel Rocha <joelxr@gmail.com>
2017-03-13 20:27:35 -0300

    finalizando aula 86

--- app/controllers/cadastro.js
+++ app/controllers/cadastro.js
@@ -1,3 +1,21 @@
 module.exports.cadastro = function (application, req, res) {
-    res.render('cadastro');
+    res.render('cadastro', {validacao: {}, dadosForm : {} });
+}
+
+module.exports.cadastrar = function (application, req, res) {
+    var dados = req.body;
+    req.assert('nome', 'Nome não pode ser vazio').notEmpty();
+    req.assert('usuario', 'Usuario não pode ser vazio').notEmpty();
+    req.assert('senha', 'Senha não pode ser vazio').notEmpty();
+    req.assert('casa', 'Casa não pode ser vazio').notEmpty();
+
+    var errors = req.validationErrors();
+
+    if (errors) {
+        res.render('cadastro', {validacao : errors, dadosForm: dados});
+        return;
+    }
+
+    res.send('podemos cadastrar');
+
 }
